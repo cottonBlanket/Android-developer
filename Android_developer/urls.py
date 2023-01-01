b5ed8e6218dbff4fbd766f09e58d032110c7c9e2
Danil <donul.mukhametshin.03@gmail.com>
2023-01-01 16:40:21 +0500

    прописал базовые контроллеры для страниц сайта

--- Android_developer/urls.py
+++ Android_developer/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='home'),
+    path('relevance/', relevance, name='relevance'),
+    path('areas/', areas, name='areas'),
+    path('competencies/', competencies, name='competencies'),
+    path('vacancies/', vacancies, name='vacancies')
 ]
--- Android_developer/views.py
+++ Android_developer/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse('MAIN PAGE')
 
+
+def relevance(request):
+    return HttpResponse('Relevance page')
+
+
+def areas(request):
+    return HttpResponse('Areas page')
+
+
+def competencies(request):
+    return HttpResponse('competencies page')
+
+
+def vacancies(request):
+    return HttpResponse('vacancies page')
+
