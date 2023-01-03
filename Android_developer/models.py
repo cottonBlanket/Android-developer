89039720289aa9b630dbcef2ccbf3762d6593e4b
Danil <donul.mukhametshin.03@gmail.com>
2023-01-03 16:51:27 +0500

    Прописал модели, создал миграции, и обновил бд

--- Android_developer/models.py
+++ Android_developer/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Statistic(models.Model):
+    key = models.CharField(max_length=25)
+    info = models.CharField(max_length=25, default='all')
+    salary = models.IntegerField()
+    count = models.FloatField()
+
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    skills = models.ManyToManyField('Skill')
+    salary = models.IntegerField(verbose_name="Оклад")
+    city = models.CharField(max_length=25, verbose_name="Город")
+    published = models.DateTimeField(verbose_name="Дата публикации")
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=63, verbose_name='Навык')
+    vacancies = models.ManyToManyField('Vacancy')
+
+# class CityStatistic(models.Model):
+#     name = models.CharField(max_length=25)
+#     salary = models.IntegerField()
+#     count = models.FloatField()
+#
+#
+# class YearStatistic(models.Model):
+#     year = models.DateField()
+#     salary = models.IntegerField()
+#     count = models.FloatField()
+
+
